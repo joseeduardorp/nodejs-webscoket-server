5e8b5570afdbd077a715776141ba872c8ad99cca
José Eduardo Rocha Peres <joseeduardorperes@gmail.com>
2024-02-11 19:38:46 -0300

    feat: lendo frame não fragmentado

--- src/public/main.js
+++ src/public/main.js
@@ -1,18 +1,45 @@
+const msgInput = document.getElementById('message-input');
+const sendBtn = document.getElementById('send-btn');
+const msgOutput = document.getElementById('message-output');
+
+const socketStatus = document.querySelector('.socket-status');
+const socketOutput = document.getElementById('socket-output');
+
 const socket = new WebSocket(`ws://${window.location.hostname}:3001`);
 
-socket.addEventListener('open', (e) => {
-	console.log('onopen:', e);
+socket.addEventListener('open', () => {
+	socket.send('cliente conectando...');
+
+	socketStatus.setAttribute('title', 'conectado');
+	socketStatus.classList.add('socket-status--connected');
 });
 
 socket.addEventListener('message', (e) => {
 	console.log('onmessage:', e);
-	socket.send('mensagem do cliente');
 });
 
 socket.addEventListener('error', (e) => {
 	console.log('onerror:', e);
+
+	socketStatus.setAttribute('title', 'desconectado');
+	socketStatus.classList.remove('socket-status--connected');
 });
 
 socket.addEventListener('close', (e) => {
 	console.log('onclose:', e);
+
+	socketStatus.setAttribute('title', 'desconectado');
+	socketStatus.classList.remove('socket-status--connected');
+});
+
+sendBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+
+	const inputValue = msgInput.value;
+
+	if (inputValue.trim()) {
+		socket.send(inputValue);
+		msgOutput.textContent = inputValue;
+		msgInput.value = '';
+	}
 });
--- src/server.js
+++ src/server.js
@@ -1,13 +1,52 @@
 const { createServer, IncomingMessage, ServerResponse } = require('node:http');
+const { Socket } = require('node:net');
 const { createHash } = require('node:crypto');
+const { StringDecoder } = require('node:string_decoder');
 
 const MAGIC_STRING = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+
+const LENGHT_BYTES = {
+	126: 2,
+	127: 8,
+};
+
+const readFrame = (frameBytes = []) => {
+	const decoder = new StringDecoder('utf-8');
+
+	const [byte1] = frameBytes.splice(0, 1); // FIN + RSV1-3 + OPCODE
+	const [byte2] = frameBytes.splice(0, 1); // MASK bit + Payload Length
+
+	let payloadLength = byte2 - 128; // byte2 - MASK bit
+
+	if (payloadLength >= 126) {
+		payloadLength = frameBytes
+			.splice(0, LENGHT_BYTES[payloadLength])
+			.reduce((acc, cur) => acc * 256 + cur, 0);
+	}
+
+	const mask = frameBytes.splice(0, 4);
+	const masked_msg = frameBytes;
+	const unmasked_msg = Buffer.from(masked_msg).map((byte, i) => {
+		return byte ^ mask[i % 4];
+	});
+
+	return byte1 === 129 ? decoder.write(unmasked_msg) : unmasked_msg;
+};
+
+const socketHandler = (socket = Socket.prototype) => {
+	socket.on('data', async (data) => {
+		const bytes = Array.from(data);
+
+		const msg = readFrame(bytes);
+		console.log(msg);
+	});
+};
 
 const handler = (
 	req = IncomingMessage.prototype,
 	res = ServerResponse.prototype
 ) => {
-	const { httpVersion, headers } = req;
+	const { httpVersion, headers, socket } = req;
 
 	const {
 		host,
@@ -49,6 +88,8 @@ const handler = (
 		'sec-websocket-accept': acceptKey,
 	});
 	res.end();
+
+	socketHandler(socket);
 };
 
 const server = createServer(handler);
