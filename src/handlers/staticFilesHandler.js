b63a67f7c4bec409bc6b8437b3b11edf248afaa0
José Eduardo Rocha Peres <joseeduardorperes@gmail.com>
2024-02-15 13:52:44 -0300

    refactor: adicionando tratamento de erros no staticFileHandler

--- src/handlers/staticFilesHandler.js
+++ src/handlers/staticFilesHandler.js
@@ -1,5 +1,5 @@
 const { IncomingMessage, OutgoingMessage } = require('node:http');
-const { readFile } = require('node:fs/promises');
+const { readFile, access, constants } = require('node:fs/promises');
 const { join } = require('node:path');
 
 const publicDir = join(__dirname, '..', 'public');
@@ -8,21 +8,29 @@ const staticFilesHandler = async (
 	req = IncomingMessage.prototype,
 	res = OutgoingMessage.prototype
 ) => {
-	const requestedFile = req.url;
-	const fileName = requestedFile === '/' ? 'index.html' : requestedFile;
-	const filepath = join(publicDir, fileName);
+	try {
+		const requestedFile = req.url;
+		const fileName = requestedFile === '/' ? 'index.html' : requestedFile;
+		const filepath = join(publicDir, fileName);
 
-	const file = await readFile(filepath);
+		await access(filepath, constants.R_OK);
 
-	if (requestedFile === '/' || requestedFile === '') {
-		res.appendHeader('Content-Type', 'text/html');
-	} else if (requestedFile.includes('.css')) {
-		res.appendHeader('Content-Type', 'text/css');
-	} else if (requestedFile.includes('.js')) {
-		res.appendHeader('Content-Type', 'text/javascript');
+		const file = await readFile(filepath);
+
+		if (requestedFile === '/' || requestedFile === '') {
+			res.appendHeader('Content-Type', 'text/html');
+		} else if (requestedFile.includes('.css')) {
+			res.appendHeader('Content-Type', 'text/css');
+		} else if (requestedFile.includes('.js')) {
+			res.appendHeader('Content-Type', 'text/javascript');
+		} else {
+			return res.end();
+		}
+
+		return res.end(file);
+	} catch (error) {
+		return res.end();
 	}
-
-	return res.end(file);
 };
 
 module.exports = staticFilesHandler;
